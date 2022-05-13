bc71dc6d19bda87f3206eeb539ec009861cd64bf
Martin Nemec <martin.nemec@editel.cz>
2022-05-13 14:54:25 +0200

    Connector.java added

---
+++ Connector.java
@@ -0,0 +1,23 @@
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class Connector {
+
+    public static void main(String[] args) throws Exception {
+        if (args.length < 1) {
+            System.err.println("No URL provided");
+            System.exit(1);
+            return;
+        }
+        URL url = new URL(args[0]);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setConnectTimeout(30000);
+        con.setReadTimeout(30000);
+        try (InputStream stream = con.getInputStream()) {
+            System.out.println(new String(stream.readAllBytes(), StandardCharsets.UTF_8));
+        }
+    }
+}
